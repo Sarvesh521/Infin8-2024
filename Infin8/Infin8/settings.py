e87ac93d2c36a46a76ef7132607afda3e10e3819
Sarvesh521 <122361969+Sarvesh521@users.noreply.github.com>
2024-01-14 23:21:50 +0530

    Modification related to Update-2

--- Infin8/Infin8/settings.py
+++ Infin8/Infin8/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sarveshkumara123@gmail.com'
-EMAIL_HOST_PASSWORD = 'hhgtuyxmiuteqgxw'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_TLS = True
