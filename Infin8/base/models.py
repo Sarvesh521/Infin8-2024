3494849d5d6ce7d56b16ee6bc51630d8558c3fd7
Sarvesh521 <122361969+Sarvesh521@users.noreply.github.com>
2024-01-14 23:09:11 +0530

    Update-2
    1. Forgot Password Functionality
    2. Email Verification on Signup

--- Infin8/base/models.py
+++ Infin8/base/models.py
@@ -6,6 +6,8 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number=models.IntegerField()
     username=models.CharField(unique=True,max_length=100)
+    email_token = models.CharField(max_length=255, default="")
+    email_verified = models.BooleanField(default=False)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     def __str__(self) -> str:
--- Infin8/base/views.py
+++ Infin8/base/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from .forms import MyUserCreationForm
 from django.contrib import messages
 from .models import User
-
+import uuid
+from .utils import send_email_token
+from django.http import HttpResponse 
 def loginPage(request): 
     page = 'login'
     if request.user.is_authenticated:
@@ -16,7 +18,7 @@ def loginPage(request):
             user = User.objects.get(email=email)
         except:
             messages.error(request, 'User does not exist')
-        user = authenticate(request, email=email, password=password)
+        user = authenticate(request, email=email, password=password , email_verified=True)
 
         if user is not None:
             login(request, user)
@@ -39,12 +41,12 @@ def registerPage(request):
         form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            if "iiitb.ac.in" in user.email: 
-                user.email = user.email.lower()
-                user.save()
-                login(request, user)
-                return redirect('participant_home')
-            messages.error(request, 'Use iiitb mail id')
+            user.email = user.email.lower()
+            user.username = user.username.lower()
+            user.email_token = str(uuid.uuid4())
+            send_email_token(user.email, user.email_token)
+            user.save()
+            messages.success(request, f'Email has been sent to {user.email}')
         else:
             messages.error(request, 'An error occurred during registration (Ensure that you are not using the same email id, this error could have been caused by that)')
 
@@ -57,3 +59,19 @@ def participant_home(request):
         'user_1':user_1,
     }
     return render(request, 'participant_home.html', context)
+
+def verify(request,token):
+    if request.user.is_authenticated:
+        messages.success(request, 'You are already logged in')
+        return redirect('participant_home')
+    try:
+        user = User.objects.get(email_token=token)
+        user.email_verified = True
+        user.save()
+        messages.success(request, 'Email verified')
+        login(request, user)
+        return redirect('participant_home')
+    except Exception as e:
+        messages.error(request, 'Invalid request')
+    
+    return HttpResponse('verify')
