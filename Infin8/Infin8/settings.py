773481d2041fcf8b1d9c2452fc8874570ebdd953
Sarvesh521 <122361969+Sarvesh521@users.noreply.github.com>
2024-02-12 17:11:12 +0530

    Database Change

--- Infin8/Infin8/settings.py
+++ Infin8/Infin8/settings.py
@@ -80,14 +80,10 @@ WSGI_APPLICATION = 'Infin8.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'infin8',
-        'USER': config('USER'),
-        'PASSWORD':config('PASSWORD'),
-        'HOST': 'localhost',  # or the hostname where your MySQL server is running
-        'PORT': '3306',      # or the port on which your MySQL server is listening
-    }
+    'default':{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
 
 
