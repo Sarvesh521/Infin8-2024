3bd77b9b3902f9e8f3fbe84fe91a6384c0a05430
Sarvesh521 <122361969+Sarvesh521@users.noreply.github.com>
2024-02-13 18:50:38 +0530

    Landed.

--- Infin8/base/utils.py
+++ Infin8/base/utils.py
@@ -1,10 +1,9 @@
 from django.core.mail import send_mail
 from django.conf import settings
-
 def send_email_token(email,token):
     try:
         subject = "Verify your email"
-        message = f"Hi click on the link to verify your email http://43.205.177.189/verify/{token}/{email}"
+        message = f"Hi click on the link to verify your email http://infin8loyalty.iiitb.net/verify/{token}/{email}"
         email_from = settings.EMAIL_HOST_USER
         recipient_list = [email,]
         send_mail(subject, message, email_from, recipient_list)
